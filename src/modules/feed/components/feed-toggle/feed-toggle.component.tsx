f90ef5385dc9ad87dce41a993a06dea8de63b0c2
Andrii Romanchuk <dr.romanchuk.a@gmail.com>
2023-09-12 18:20:15 +0300

    Added Feed toggle

---
+++ src/modules/feed/components/feed-toggle/feed-toggle.component.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+type FeedToggleProps = {}
+
+const FeedToggle: React.FC<FeedToggleProps> = () => {
+  return (
+    <div className=''>
+      <ul>
+         <li>
+            <NavLink to='/' className='bg-white border-b-2 border-conduit-green hover:no-underline py-2 px-4'>Global feed</NavLink>
+         </li>
+      </ul>
+    </div>
+  )
+}
+
+export default FeedToggle
