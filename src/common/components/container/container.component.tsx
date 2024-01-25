d833be692282dbce789152128afbec58c7ab959f
Andrii Romanchuk <dr.romanchuk.a@gmail.com>
2024-01-25 13:40:58 +0200

    Changed Container styles

--- src/common/components/container/container.component.tsx
+++ src/common/components/container/container.component.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {}
 
 const Container: FC<PropsWithChildren<ContainerProps>> = ({ children }) => {
   return (
-   <div className='container mx-auto px-20'>
+   <div className='container mx-auto px-5 md:px-20 '>
       { children }
    </div>
   )
