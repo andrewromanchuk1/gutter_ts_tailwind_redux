6bc8cac2b208282db3abd7f83732f3cb63963b87
Andrii Romanchuk <dr.romanchuk.a@gmail.com>
2024-02-16 13:13:10 +0200

    test

--- src/common/components/textarea/textarea.component.tsx
+++ src/common/components/textarea/textarea.component.tsx
@@ -1,40 +1,36 @@
-import clsx from 'clsx';
-import { ComponentProps, forwardRef } from 'react'
+import clsx from "clsx";
+import { ComponentProps, forwardRef } from "react";
 
 enum InputSize {
-   SM = 'SM',
-   BASE = 'BASE',
+  SM = "SM",
+  BASE = "BASE",
 }
 
 type InputProps = {
-   placeholder: ComponentProps<'textarea'>['placeholder'];
-   name: ComponentProps<'textarea'>['name'];
-   onChange: ComponentProps<'textarea'>['onChange'];
-   onBlur: ComponentProps<'textarea'>['onBlur'];
-   rows?: ComponentProps<'textarea'>['rows'];
-   className?: ComponentProps<'textarea'>['className'];
-   size?: keyof typeof InputSize;
-   noBorder?: boolean;
-}
+  placeholder: ComponentProps<"textarea">["placeholder"];
+  name: ComponentProps<"textarea">["name"];
+  onChange: ComponentProps<"textarea">["onChange"];
+  onBlur: ComponentProps<"textarea">["onBlur"];
+  rows?: ComponentProps<"textarea">["rows"];
+  className?: ComponentProps<"textarea">["className"];
+  size?: keyof typeof InputSize;
+  noBorder?: boolean;
+};
 
 const TextArea = forwardRef<HTMLTextAreaElement, InputProps>(
-   ({size = InputSize.BASE, noBorder, className, ...inputProps }, ref) => {
-      const inputClasses = clsx(
-         'border border-black/15 rounded w-full outline-none', {
-            'py-3 px-6 text-xl': size === InputSize.BASE,
-            'py-1 px-2 text-base': size === InputSize.SM,
-            'border-0': noBorder,
-         },
-         className,
-      )
+  ({ size = InputSize.BASE, noBorder, className, ...inputProps }, ref) => {
+    const inputClasses = clsx(
+      "border border-black/15 rounded w-full outline-none",
+      {
+        "py-3 px-6 text-xl": size === InputSize.BASE,
+        "py-1 px-2 text-base": size === InputSize.SM,
+        "border-0": noBorder,
+      },
+      className
+    );
 
-      return (
-         <textarea
-            ref={ref}
-            {...inputProps}
-            className={inputClasses}
-         />
-      )
-})
+    return <textarea ref={ref} {...inputProps} className={inputClasses} />;
+  }
+);
 
-export default TextArea
+export default TextArea;
